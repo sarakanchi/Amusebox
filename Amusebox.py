efd4250ccf4752027d9f77acc738aae739b65288
sarakanchi <78954236+sarakanchi@users.noreply.github.com>
2023-10-16 16:38:59 +0530

    Update Amusebox.py

--- Amusebox.py
+++ Amusebox.py
@@ -10,9 +10,16 @@ import datetime
 from base64 import b64decode,b64encode
 from datetime import date
 
-expirydate = datetime.date(2024, 12, 12)
-#expirydate = datetime.date(2021,20, 30)
+expirydate = datetime.date(2022, 12, 18)
+#expirydate = datetime.date(2021, 11, 30)
 today=date.today()
+green="\033[3;32m"
+neon="\033[3;36m"
+nc="\033[00m"
+red="\033[3;31m"
+purple="\033[3;34m"
+yellow="\033[3;33m"
+voilet="\033[3;35m"
 def hero():
 
     def chalo():
@@ -22,7 +29,7 @@ def hero():
             for c in itertools.cycle(['|', '/', '-', '\\']) :
                 if done:
                     break
-                sys.stdout.write('\rhacking in the parity server for next colour--------- ' + c)
+                sys.stdout.write('\rconnecting to server for next colour--------- ' + c)
                 sys.stdout.flush()
                 time.sleep(0.1)
             sys.stdout.write('\rDone!     ')
@@ -64,22 +71,21 @@ def hero():
     clear()
     y=1
     newperiod=period
-    banner='figlet AMUSEBOX'
-    m=0
-    i=1
-    thisway=[1,2,4,6,7,8,15,14,16,17,18]
-    thatway=[3,5,9,10,11,12,13,19,20]
+    banner='figlet RXCE 8.1|lolcat'
+    thisway=[2,6,8,11,12,15,16,18,19,20]
+    thatway=[1,3,4,5,7,9,10,14,13,17]
     numbers=[]
+    i=1
     while(y):
         clear()
         system(banner)
-        print("Contact me on telegram @Hacker6363")
-        print("Enter ",newperiod," Parity Price :")
+        print(f"{red}Contact me on telegram @Mr_kumar1")
+        print(f"{yellow}Enter ",newperiod," Price :")
         current=input()
         current=int(current)
-        #chalo()
-        print("\n---------Successfully hacked the server-----------")
-        #chalo1()
+        chalo()
+        print("\n---------Successfully Connected to the server-----------")
+        chalo1()
         print("\n---------Successfully got the colour -------------")
         print('\n')
         def getSum(n):
@@ -92,40 +98,42 @@ def hero():
             n=int(current)%10
             if((m%2==0 and n%2==0) or (m%2==1 and n%2==1)):
                 if current in numbers:
-                    print(newperiod+1," : RED")
+                    print(newperiod+1," : 💥🟢GREEN1🟢💥")
                 else:
-                    print(newperiod+1," : GREEN")
+                    print(newperiod+1," : 💥🔴RED2🔴💥")
             else:
                 if current in numbers:
-                    print(newperiod+1," : GREEN")
+                    print(newperiod+1," :  💥🔴RED2🔴💥")
                 else:
-                    print(newperiod+1," : RED")
+                    print(newperiod+1," : 💥🟢GREEN1🟢💥")
         if i in thatway:
             m=getSum(current)+1
             n=int(current)%10
             if((m%2==0 and n%2==0) or (m%2==1 and n%2==1)):
                 if current in numbers:
-                    print(newperiod+1,": RED")
+                    print(newperiod+1,": 💥💥🟢GREEN2🟢💥💥")
                 else:
-                    print(newperiod+1,": GREEN")
+                    print(newperiod+1,": 💥💥🔴RED6🔴💥💥")
             else:
                 if current in numbers:
-                    print(newperiod+1,": GREEN")
+                    print(newperiod+1,": 💥💥🟢GREEN3🟢💥💥")
                 else:
-                    print(newperiod+1,": RED")
+                    print(newperiod+1,": 💥💥🔴RED4🔴💥💥")
         i=i+1
         newperiod+=1
         numbers.append(current)
         y=input("Do you want to play : Press 1 and 0 to exit \n")
         if(y==0):
             y=False
-        if (len(numbers)>21):
+        if (len(numbers)>15):
             clear()
             system('figlet Thank you!!')
             print("Play on next specified time!!")
             print("-----------Current Time UP----------")
-            sys.exit(" \n \n \n Contact on Telegram @Hacker6363")
-            #print(numbers)
+            sys.exit(" \n \n \n Contact on Telegram @Mr_kumar1")
+            print(numbers)
+  
+
 
 
 if(expirydate>today):
@@ -134,37 +142,49 @@ if(expirydate>today):
     Firstend = now.replace(hour=11, minute=35, second=0, microsecond=0)
     Second = now.replace(hour=13, minute=55, second=0, microsecond=0)
     Secondend = now.replace(hour=14, minute=35, second=0, microsecond=0)
-    Third = now.replace(hour=16, minute=55, second=0, microsecond=0)
-    Thirdend = now.replace(hour=17, minute=35, second=0, microsecond=0)
-    Final = now.replace(hour=19, minute=55, second=0, microsecond=0)
-    Finalend = now.replace(hour=20, minute=35, second=0, microsecond=0)
+    Third = now.replace(hour=14, minute=25, second=0, microsecond=0)
+    Thirdend = now.replace(hour=16, minute=35, second=0, microsecond=0)
+    Final = now.replace(hour=20, minute=55, second=0, microsecond=0)
+    Finalend = now.replace(hour=21, minute=35, second=0, microsecond= 0)
+    FFinal = now.replace(hour=22, minute=55, second=0, microsecond= 0)
+    FFinalend = now.replace(hour=23, minute=35, second=0, microsecond= 0)
 
-    if (True):
-            period=385
+    if (now>First and now<Firstend):
+            period=220
             hero()
-    elif(False):
-            period=342
+    elif(now>Second and now<Secondend):
+            period=280
             hero()
-    elif(False):
-            period=343
+    elif(now>Third and now<Thirdend):
+            period=300
             hero()
     elif(now>Final and now<Finalend):
-            period=400
+            period=420
+            hero()
+    elif(now>FFinal and now<FFinalend):
+            period=460
             hero()
     else:
-        banner='figlet AMUSEBOX'
-        print("Hi!! Thanks for buying the hack")
+        banner='figlet RXCE 8.1|lolcat'
+        print("Hi!! Thanks for buying Life time the hack")
         print("----------Your play time-----------")
-        print(" Yesterday Server Error , So Play today" )
-        print("2nd Sept 2021, 08:00 PM- 08:30 PM")
+        print(" 11:00 PM- 11:35 PM")
+        print(" 02:00 PM- 02:35 PM")
+        print(" 03:00 PM- 03:35 PM")
+        print(" 09:00 PM- 09:35 PM")
+        print(" 11:00 PM- 12:35 PM")
         print("Please play on the given time, and ")
         print("If you think it is an error contact")
-        print(" admin on telegram @Hacker6363 ")
-
-
-
+        print(" admin on telegram @Mr_kumar1 ")
+    
 else:
-    banner='figlet AMUSEBOX'
+    banner='figlet Thank '
     system(banner)
+    print("*---------*----------*-------------*----------*")
     print("Your hack has expired--- Please contact")
-    print(" on telegram -----------@Hacker6363")
+    print(" on telegram ----@Mr_kumar1 for activating")
+    print(" Recharge Amount :        Total limit " )
+    print(" 2.     3000 INR -------  30 Days")
+    print("*---------*----------*-------------*----------*")
+    print("Your custom hack can be made request from us.")
+    print( "Msg me on telegram @Mr_kumar1")
